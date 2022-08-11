37a65205aaf4fa2f3a2d9d73761fad0421aca976
Huzaifa Luqman <109826626+huzaifaluqman@users.noreply.github.com>
2022-08-11 00:11:09 -0700

    Add files via upload

---
+++ food up/script.js
@@ -0,0 +1,136 @@
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header .navbar a");
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  loginForm.classList.remove("active");
+
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header .navbar a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+document.querySelector("#search-icon").onclick = () => {
+  document.querySelector("#search-form").classList.toggle("active");
+};
+
+document.querySelector("#close").onclick = () => {
+  document.querySelector("#search-form").classList.remove("active");
+};
+let search = document.getElementById("search-box");
+search.addEventListener("input", function () {
+  let inputVal = search.value.toLowerCase();
+  let noteCard = document.getElementsByClassName("box");
+
+  console.log("Input event fired!", inputVal);
+  Array.from(noteCard).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("h3");
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+});
+
+let loginForm = document.querySelector(".login-form-container");
+let formBtn = document.querySelector("#login-btn");
+formBtn.addEventListener("click", () => {
+  loginForm.classList.add("active");
+});
+let formClose = document.querySelector("#form-close");
+formClose.addEventListener("click", () => {
+  loginForm.classList.remove("active");
+});
+
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+let countDate = new Date("August 13, 2022 00:00:00").getTime();
+
+function CountDown() {
+  let now = new Date().getTime();
+  gap = countDate - now;
+
+  let second = 1000;
+  let minute = second * 60;
+  let hour = minute * 60;
+  let day = hour * 24;
+
+  let d = Math.floor(gap / day);
+  let h = Math.floor((gap % day) / hour);
+  let m = Math.floor((gap % hour) / minute);
+  let s = Math.floor((gap % minute) / second);
+
+  document.getElementById("day").innerText = d;
+  document.getElementById("hour").innerText = h;
+  document.getElementById("minute").innerText = m;
+  document.getElementById("second").innerText = s;
+}
+
+setInterval(function () {
+  CountDown();
+}, 1000);
+
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut() {
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
